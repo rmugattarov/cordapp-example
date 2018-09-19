3c7de2256b31fc70a0569dd9909e3a504f6bb0cb
rmugattarov <mugattarov@gmail.com>
2018-09-19 21:52:11 +0300

    Implementted double encryption with Diffie-Hellman.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-flow start ExampleFlow$Initiator sum: 50, otherParty: "O=Acceptor,L=New York,C=US"
+flow start ExampleFlow$Initiator otherParty: "O=Acceptor,L=New York,C=US"
 run vaultQuery contractStateType: com.example.state.SummationState
--- java-source/src/main/java/com/example/flow/ExampleFlow.java
+++ java-source/src/main/java/com/example/flow/ExampleFlow.java
@@ -13,15 +13,28 @@ import net.corda.core.transactions.SignedTransaction;
 import net.corda.core.transactions.TransactionBuilder;
 import net.corda.core.utilities.ProgressTracker;
 import net.corda.core.utilities.ProgressTracker.Step;
+import net.corda.core.utilities.UntrustworthyData;
+
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.function.BiFunction;
 
 import static com.example.contract.SummationContract.SUMMATION_CONTRACT_ID;
 
 public class ExampleFlow {
+
+    public static final UntrustworthyData.Validator<Object, Object> VALIDATOR = (UntrustworthyData.Validator<Object, Object>) data -> data;
+    public static final BiFunction<Integer, Integer, Integer> ENCRYPT = (data , key) -> data + key;
+    public static final BiFunction<Integer, Integer, Integer> DECRYPT = (data , key) -> data - key;
+
     @InitiatingFlow
     @StartableByRPC
     public static class Initiator extends FlowLogic<SignedTransaction> {
+        private static final int ENCRYPTED_VALUE_TWO = 44;
+        private static final int P = 23;
+        private static final int G = 5;
+        private int diffieKey;
 
-        private final int sum;
+        private int encryptedSum;
         private final Party otherParty;
 
         private final Step GENERATING_TRANSACTION = new Step("Generating transaction based on new input.");
@@ -48,8 +61,7 @@ public class ExampleFlow {
                 FINALISING_TRANSACTION
         );
 
-        public Initiator(int sum, Party otherParty) {
-            this.sum = sum;
+        public Initiator(Party otherParty) {
             this.otherParty = otherParty;
         }
 
@@ -64,14 +76,28 @@ public class ExampleFlow {
             // Obtain a reference to the notary we want to use.
             final Party notary = getServiceHub().getNetworkMapCache().getNotaryIdentities().get(0);
 
-//            String response = subFlow(new SendHelloFlow(otherParty));
-//            System.out.println("!!! received : " + response);
+            FlowSession otherPartySession = initiateFlow(otherParty);
+
+            int a = ThreadLocalRandom.current().nextInt(10);
+            System.out.println("*** a: " + a);
+            int A = (int) (Math.pow(G, a) % P);
+            System.out.println("*** A: " + A);
+            int B = (int) otherPartySession.sendAndReceive(Integer.class, A).unwrap(VALIDATOR);
+            System.out.println("*** B: " + B);
+            diffieKey = (int) (Math.pow(B, a) % P);
+            System.out.println("*** Diffie Key: " + diffieKey);
+            int doubleEncryptedOperand = ENCRYPT.apply(ENCRYPTED_VALUE_TWO, diffieKey);
+            System.out.println("*** Double encrypted operand: " + doubleEncryptedOperand);
+            int doubleEncryptedSum = (int) otherPartySession.sendAndReceive(Integer.class, doubleEncryptedOperand).unwrap(VALIDATOR);
+            System.out.println("*** Double encrypted sum:" + doubleEncryptedSum);
+            encryptedSum = DECRYPT.apply(doubleEncryptedSum, diffieKey);
+            System.out.println("*** Encrypted sum:" + encryptedSum);
 
             // Stage 1.
             progressTracker.setCurrentStep(GENERATING_TRANSACTION);
             // Generate an unsigned transaction.
             Party me = getServiceHub().getMyInfo().getLegalIdentities().get(0);
-            SummationState summationState = new SummationState(sum, me, otherParty, new UniqueIdentifier());
+            SummationState summationState = new SummationState(encryptedSum, me, otherParty, new UniqueIdentifier());
             final Command<SummationContract.Commands.Create> txCommand = new Command<>(
                     new SummationContract.Commands.Create(),
                     ImmutableList.of(summationState.getInitiator().getOwningKey(), summationState.getAcceptor().getOwningKey()));
@@ -92,7 +118,6 @@ public class ExampleFlow {
             // Stage 4.
             progressTracker.setCurrentStep(GATHERING_SIGS);
             // Send the state to the counterparty, and receive it back with their signature.
-            FlowSession otherPartySession = initiateFlow(otherParty);
             final SignedTransaction fullySignedTx = subFlow(
                     new CollectSignaturesFlow(partSignedTx, ImmutableSet.of(otherPartySession), CollectSignaturesFlow.Companion.tracker()));
 
@@ -105,6 +130,12 @@ public class ExampleFlow {
 
     @InitiatedBy(Initiator.class)
     public static class Acceptor extends FlowLogic<SignedTransaction> {
+        private static final int ENCRYPTION_KEY = 42;
+        private static final int SECOND_SUMMATION_OPERAND = 3;
+        private static final int P = 23;
+        private static final int G = 5;
+        private int diffieKey;
+
 
         private final FlowSession otherPartyFlow;
 
@@ -115,6 +146,32 @@ public class ExampleFlow {
         @Suspendable
         @Override
         public SignedTransaction call() throws FlowException {
+
+            int A = (int) otherPartyFlow.receive(Integer.class).unwrap(VALIDATOR);
+
+            System.out.println("*** A:" + A);
+            int b = ThreadLocalRandom.current().nextInt(10);
+            System.out.println("*** b:" + b);
+            int B = (int) (Math.pow(G, b) % P);
+            System.out.println("*** B:" + B);
+            diffieKey = (int) Math.pow(A, b) % P;
+            System.out.println("*** Diffie Key:" + diffieKey);
+
+            int doubleEncryptedOperand = (int) otherPartyFlow.sendAndReceive(Integer.class, B).unwrap(VALIDATOR);
+
+            System.out.println("*** Double encrypted operand: " + doubleEncryptedOperand);
+            int encryptedOperand = DECRYPT.apply(doubleEncryptedOperand, diffieKey);
+            System.out.println("*** Encrypted operand:" + encryptedOperand);
+            int decryptedOperand = DECRYPT.apply(encryptedOperand, ENCRYPTION_KEY);
+            System.out.println("*** Decrypted operand: " + decryptedOperand);
+            int sum = decryptedOperand + SECOND_SUMMATION_OPERAND;
+            System.out.println("*** Sum: " + sum);
+            int encryptedSum = ENCRYPT.apply(sum, ENCRYPTION_KEY);
+            System.out.println("*** Encrypted sum: " + encryptedSum);
+            int doubleEncryptedSUm = ENCRYPT.apply(encryptedSum, diffieKey);
+            System.out.println("*** Double encrypted sum: " + doubleEncryptedSUm);
+
+            otherPartyFlow.send(doubleEncryptedSUm);
 
             class SignTxFlow extends SignTransactionFlow {
                 private SignTxFlow(FlowSession otherPartyFlow, ProgressTracker progressTracker) {
@@ -131,39 +188,4 @@ public class ExampleFlow {
     }
 
 
-    @InitiatingFlow
-    public static class SendHelloFlow extends FlowLogic<String> {
-
-        private final Party otherParty;
-
-        public SendHelloFlow(Party otherParty) {
-            this.otherParty = otherParty;
-        }
-
-        @Override
-        @Suspendable
-        public String call() throws FlowException {
-            FlowSession flowSession = initiateFlow(otherParty);
-            String response = flowSession.sendAndReceive(String.class, "Hello!").unwrap(s -> s);
-            System.out.println("!!! response : " + response);
-            return response;
-        }
-    }
-
-    @InitiatedBy(SendHelloFlow.class)
-    public static class RespondWithHelloFlow extends FlowLogic<String> {
-        private final FlowSession otherPartyFlow;
-
-        public RespondWithHelloFlow(FlowSession otherPartyFlow) {
-            this.otherPartyFlow = otherPartyFlow;
-        }
-
-        @Override
-        @Suspendable
-        public String call() throws FlowException {
-            String received = otherPartyFlow.receive(String.class).unwrap(s -> s);
-            System.out.println("!!! received : " + received);
-            return "Hello yourself!";
-        }
-    }
 }
